e1c8acf23a9534894ab758fe7f0063679c02e0da
Jiri Srba <srba@cs.aau.dk>
2014-02-14 21:29:53 +0100

    Access to net matrix and marking

--- VerifyPN.cpp
+++ VerifyPN.cpp
@@ -270,8 +270,20 @@ int main(int argc, char* argv[]){
         
         if (enablereduction) {
             fprintf(stderr,"Net reduction enabled.\n");
-            fprintf(stderr,"Number of places: %i\n\n",net->numberOfPlaces());
+            fprintf(stderr,"Number of places: %i\n",net->numberOfPlaces());
+            fprintf(stderr,"Number of transitions: %i\n\n",net->numberOfTransitions());
            
+            for (int i=0; i < net->numberOfPlaces(); i++) {
+                for (int j=0; j < net->numberOfTransitions(); j++) {
+                     fprintf(stderr,"Input arc from place %i to transitions %i: %i\n",i,j,net->inArc(i,j));   
+                     fprintf(stderr,"Output arc from transition %i to place %i: %i\n\n",j,i,net->outArc(j,i));  
+                }
+            }
+            
+            for (int i=0; i < net->numberOfPlaces(); i++) {
+                fprintf(stderr,"Marking at place %i is: %i\n",i,m0[i]);
+            } 
+            
         }
         
 	//----------------------- Reachability -----------------------//
