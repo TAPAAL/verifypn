bbf47b9208b063c783c7c098011607a0233cee5a
Jiri Srba <srba@cs.aau.dk>
2014-02-17 09:50:29 +0100

    stdout used instead of stderr for debugging info

--- PetriEngine/Reducer.cpp
+++ PetriEngine/Reducer.cpp
@@ -9,23 +9,23 @@
 #include "PetriNet.h"
 
 namespace PetriEngine{
- 
+    
     void Reducer::Print(PetriNet* net, MarkVal* m0){
-        fprintf(stderr,"Net reduction enabled.\n");
-            fprintf(stderr,"Number of places: %i\n",net->numberOfPlaces());
-            fprintf(stderr,"Number of transitions: %i\n\n",net->numberOfTransitions());
+        fprintf(stdout,"Net reduction enabled.\n");
+            fprintf(stdout,"Number of places: %i\n",net->numberOfPlaces());
+            fprintf(stdout,"Number of transitions: %i\n\n",net->numberOfTransitions());
            
             for (int i=0; i < net->numberOfPlaces(); i++) {
                 for (int j=0; j < net->numberOfTransitions(); j++) {
-                     fprintf(stderr,"Input arc from place %i to transitions %i: %i\n",i,j,net->inArc(i,j));   
-                     fprintf(stderr,"Output arc from transition %i to place %i: %i\n\n",j,i,net->outArc(j,i));  
+                     fprintf(stdout,"Input arc from place %i to transitions %i: %i\n",i,j,net->inArc(i,j));   
+                     fprintf(stdout,"Output arc from transition %i to place %i: %i\n\n",j,i,net->outArc(j,i));  
                 }
             }
             
             for (int i=0; i < net->numberOfPlaces(); i++) {
-                fprintf(stderr,"Marking at place %i is: %i\n",i,m0[i]);
+                fprintf(stdout,"Marking at place %i is: %i\n",i,m0[i]);
             } 
-            fprintf(stderr,"\n");
+            fprintf(stdout,"\n");
     }
     
 }
