9d8730b42e7437229c6383788d775aa2c787fca9
Jiri Srba <srba@cs.aau.dk>
2014-03-13 21:49:15 +0100

    work on parsing XML queries

--- PetriParse/QueryXMLParser.cpp
+++ PetriParse/QueryXMLParser.cpp
@@ -11,24 +11,59 @@ using namespace std;
 bool QueryXMLParser::parse(const std::string& xml){
 	//Parser the xml
 	DOMElement* root = DOMElement::loadXML(xml);
-	parseElement(root);
+	parsePropertySet(root);
 
 	//Release DOM tree
 	delete root;
 	return true;
 }
 
-void QueryXMLParser::parseElement(DOMElement* element){
-	cout << (element)->getElementName() << endl;
+
+void QueryXMLParser::parsePropertySet(DOMElement* element){
+	if (element->getElementName()!="property-set") {
+		cout << "ERROR missing property-set" << endl;
+	}
 	DOMElements elements = element->getChilds();
 	DOMElements::iterator it;
 	for(it = elements.begin(); it != elements.end(); it++){
-		if((*it)->getElementName() == "id"){
-			cout << (*it)->getCData() << endl;
-			parseElement(*it);
-		} else {
-			parseElement(*it);
-		}	
+		parseProperty(*it);
 	}
+}
+
+void QueryXMLParser::parseProperty(DOMElement* element){
+	if (element->getElementName()!="property") {
+		cout << "ERROR missing property" << endl;
+	}
+	DOMElements elements = element->getChilds();
+	DOMElements::iterator it;
+	for(it = elements.begin(); it != elements.end(); it++){
+	//	cout << (*it)->getElementName() << endl;
+		if ((*it)->getElementName()=="id") {
+			cout << "ID: " << (*it)->getCData() << endl;
+		} else if ((*it)->getElementName()=="formula") {
+			parseFormula(*it);
+		} else if ((*it)->getElementName()=="tags") {
+			parseTags(*it);
+		}
+	}
+	cout << endl;
+}
+
+void QueryXMLParser::parseTags(DOMElement* element){
+	DOMElements elements = element->getChilds();
+	DOMElements::iterator it;
+	for(it = elements.begin(); it != elements.end(); it++){
+		cout << (*it)->getElementName() << endl;
+	}
+	cout << endl;
+}
+
+void QueryXMLParser::parseFormula(DOMElement* element){
+	DOMElements elements = element->getChilds();
+	DOMElements::iterator it;
+	for(it = elements.begin(); it != elements.end(); it++){
+	//	cout << (*it)->getElementName() << endl;
+	}
+	cout << endl;
 }
 
--- PetriParse/QueryXMLParser.h
+++ PetriParse/QueryXMLParser.h
@@ -22,8 +22,10 @@ public:
 	bool parse(const std::string& xml);
         
 private:
-	void parseElement(XMLSP::DOMElement* element);
-	
+        void parsePropertySet(XMLSP::DOMElement* element);
+        void parseProperty(XMLSP::DOMElement* element);
+        void parseTags(XMLSP::DOMElement* element);
+        void parseFormula(XMLSP::DOMElement* element);
 };
 
 #endif	/* QUERYXMLPARSER_H */
