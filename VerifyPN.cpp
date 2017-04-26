7dc31fc749b69c276a1e91eafd55482ca86e13a6
Lasse S.J. <lasjen12@student.aau.dk>
2017-04-26 10:27:00 +0200

    Updated CTL SS verification. BFS will now be rejected when using the ctl engine.

--- VerifyPN.cpp
+++ VerifyPN.cpp
@@ -327,8 +327,8 @@ ReturnValue parseOptions(int argc, char* argv[], options_t& options)
         if(options.strategy == PetriEngine::Reachability::HEUR){
             options.strategy = PetriEngine::Reachability::DFS;
         }
-        else if(options.strategy != PetriEngine::Reachability::DFS && options.strategy != PetriEngine::Reachability::BFS){
-            std::cerr << "Argument Error: Invalid CTL search strategy. Valid ones are DFS (default) and BFS." << std::endl;
+        else if(options.strategy != PetriEngine::Reachability::DFS){
+            std::cerr << "Argument Error: Invalid CTL search strategy. Only DFS is supported by CTL engine." << std::endl;
             return ErrorCode;
         }
     }
