2c24ec4561c4d5222e45bcfbe733fd9bbde0abb6
Jiri Srba <srba.jiri@gmail.com>
2017-04-27 12:45:35 +0200

    updated BenchKit head

--- Scripts/MCC17/competition-scripts/BenchKit_head.sh
+++ Scripts/MCC17/competition-scripts/BenchKit_head.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # This is the initialization script for the participation of TAPAAL
-# untimed sequntial engine verifypn in the Petri net competition 2016.
+# untimed sequntial engine verifypn in the Petri net competition 2017.
 
 # BK_EXAMINATION: it is a string that identifies your "examination"
 # BK_TOOL: it is the name of the TAPAAL tool variant to be invoked
 # export PATH="$PATH:/home/mcc/BenchKit/bin/"
 
-PREFIX=$HOME/Datalogi/speciale/tools/flagship/Scripts/MCC17/test-scripts
+PREFIX=/home/mcc/BenchKit
 
 case "$BK_TOOL" in
-    tapaal)
+    *)
         echo "---> " $BK_TOOL " --- TAPAAL"
         $PREFIX/tapaal.sh
                 ;;
-    *)
+    other)
         echo "---> Error: Unrecognized BK_TOOL name !!!"  
         exit 0
         ;;
