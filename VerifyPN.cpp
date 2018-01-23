0d108f5bf1fb13726e3122791b9a020e66c16bfc
Peter G. Jensen <peter.gjoel@gmail.com>
2018-01-23 11:01:04 +0100

    using return flags

--- VerifyPN.cpp
+++ VerifyPN.cpp
@@ -553,7 +553,7 @@ int main(int argc, char* argv[]) {
     if(queries.size() == 0 || contextAnalysis(b2, qnet.get(), queries) != ContinueCode)  return ErrorCode;
 
     if (options.strategy == PetriEngine::Reachability::OverApprox && options.queryReductionTimeout == 0) // Conflicting flags "-s OverApprox" and "-q 0"
-        return 0;
+        return ErrorCode;
 
     // simplification. We always want to do negation-push and initial marking check.
     {
@@ -743,7 +743,7 @@ int main(int argc, char* argv[]) {
         }
         
         if (std::find(results.begin(), results.end(), ResultPrinter::Unknown) == results.end()) {
-            return 0;
+            return SuccessCode;
         }
     }
     
@@ -766,6 +766,6 @@ int main(int argc, char* argv[]) {
             options.printstatistics, 
             options.trace);
        
-    return 0;
+    return SuccessCode;
 }
 
