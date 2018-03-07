c01f965a2a1e148fb31b788b162c31f5e868796c
Peter G. Jensen <peter.gjoel@gmail.com>
2018-03-07 21:52:15 +0100

    copied over old implementation and modified to fit with new structure

--- CTL/SearchStrategy/SearchStrategy.cpp
+++ CTL/SearchStrategy/SearchStrategy.cpp
@@ -7,69 +7,12 @@
 
 
 namespace SearchStrategy{
-    bool SearchStrategy::trivialNegation()
-    {
-        for(auto it = N.begin(); it != N.end(); ++it)
-        {
-            bool allOne = true;
-            bool hasCZero = false;
-            auto e = *it;
-            for (DependencyGraph::Configuration *c : e->targets) {
-                if (c->assignment == DependencyGraph::Assignment::CZERO) {
-                    hasCZero = true;
-                    break;
-                }
-                if (c->assignment != DependencyGraph::Assignment::ONE) {
-                    allOne = false;
-                }
-            }
 
-            if(allOne || hasCZero)
-            {
-                D.push_back(*it);
-                it = N.erase(it);
-                if(N.empty() || it == N.end()) break;
-            }
-        }
-        return !D.empty();
-    }
- 
-    
-    void SearchStrategy::releaseNegationEdges(uint32_t dist)
+    bool SearchStrategy::empty() const
     {
-        for(auto it = N.begin(); it != N.end(); ++it)
-        {
-            assert(*it);
-            if((*it)->source->getDistance() >= dist || (*it)->source->isDone())
-            {
-                pushEdge(*it);
-                it = N.erase(it);
-                if(N.empty() || it == N.end()) break;
-            }
-        }
+        return Wsize() == 0 && N.empty() && D.empty();
     }
-    
-    
-    uint32_t SearchStrategy::maxDistance() const
-    {
-        uint32_t m = 0;
-        for(DependencyGraph::Edge* e : N)
-        {
-            if(!e->source->isDone())
-                m = std::max(m, e->source->getDistance());
-        }
-        return m;
-    }
-    
 
-    void SearchStrategy::pushDependency(DependencyGraph::Edge* edge)
-    {
-        if(edge->source->isDone()) return;
-        edge->status = 2;
-        ++edge->refcnt;
-        D.push_back(edge);
-    }
-    
     void SearchStrategy::pushNegation(DependencyGraph::Edge* edge)
     {
         edge->status = 3;
@@ -97,35 +40,6 @@ namespace SearchStrategy{
         }
     }
 
-    bool SearchStrategy::empty() const
-    {
-        return D.empty() && N.empty() && Wsize() == 0;
-    }
-    
-    bool SearchStrategy::available() const 
-    {
-        return !D.empty() || Wsize() > 0;
-    }
-    
-    DependencyGraph::Edge* SearchStrategy::popEdge(bool saturate) {
-        if(!D.empty())
-        {
-            auto e = D.back();
-            D.pop_back();
-            return e;
-        }
-        if(saturate)
-            return nullptr;
-
-        if(Wsize() == 0)
-            return nullptr;
-        return popFromW();
-    }
-
-    size_t SearchStrategy::size() const {
-        return D.size() + N.size() + Wsize();
-    }
-
     void SearchStrategy::pushEdge(DependencyGraph::Edge *edge)
     {
         if(edge->status > 0 || edge->source->isDone()) return;
@@ -139,6 +53,88 @@ namespace SearchStrategy{
         pushToW(edge);
     }
 
+    void SearchStrategy::pushDependency(DependencyGraph::Edge* edge)
+    {
+        if(edge->source->isDone()) return;
+        edge->status = 2;
+        ++edge->refcnt;
+        D.push_back(edge);
+    }
 
-    
+    DependencyGraph::Edge* SearchStrategy::popEdge(bool saturate)
+    {
+        if(saturate && D.empty()) return nullptr;
+
+        if (Wsize() == 0 && D.empty()) {
+            return nullptr;
+        }
+
+        auto edge = D.empty() ? popFromW() : D.back();
+
+        if(!D.empty())
+            D.pop_back();
+
+        assert(edge->refcnt >= 0);
+        --edge->refcnt;
+        edge->status = 0;
+        return edge;
+    }
+
+    uint32_t SearchStrategy::maxDistance() const
+    {
+        uint32_t m = 0;
+        for(DependencyGraph::Edge* e : N)
+        {
+            if(!e->source->isDone())
+                m = std::max(m, e->source->getDistance());
+        }
+        return m;
+    }
+
+    bool SearchStrategy::available() const
+    {
+        return Wsize() > 0 || !D.empty();
+    }
+
+    void SearchStrategy::releaseNegationEdges(uint32_t dist)
+    {
+        for(auto it = N.begin(); it != N.end(); ++it)
+        {
+            assert(*it);
+            if((*it)->source->getDistance() >= dist || (*it)->source->isDone())
+            {
+                pushToW(*it);
+                assert(W.front()->weight >= W.back()->weight);
+                it = N.erase(it);
+                if(N.empty() || it == N.end()) break;
+            }
+        }
+    }
+
+    bool SearchStrategy::trivialNegation()
+    {
+        for(auto it = N.begin(); it != N.end(); ++it)
+        {
+            bool allOne = true;
+            bool hasCZero = false;
+            auto e = *it;
+            for (DependencyGraph::Configuration *c : e->targets) {
+                if (c->assignment == DependencyGraph::Assignment::CZERO) {
+                    hasCZero = true;
+                    break;
+                }
+                if (c->assignment != DependencyGraph::Assignment::ONE) {
+                    allOne = false;
+                }
+            }
+
+            if(allOne || hasCZero)
+            {
+                D.push_back(*it);
+                it = N.erase(it);
+                if(N.empty() || it == N.end()) break;
+            }
+        }
+        return !D.empty();
+    }
 }
