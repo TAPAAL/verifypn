520968232a5765edddf6154343e8780e927d4540
Andreas Klostergaard <aklost11@student.aau.dk>
2018-04-19 23:35:32 +0200

    Fixed comparison script

--- Scripts/Comparison/compare.sh
+++ Scripts/Comparison/compare.sh
@@ -2,7 +2,7 @@
 
 #ENDING="UpperBounds"
 RESULT_VECTOR=${RESULT_VECTOR:-"0 1 10 11 12 13 14 15 2 3 4 5 6 7 8 9"}
-RESULT_FILE=${RESULT_FILE:-"answers/mcc17.csv"}
+RESULT_FILE=${RESULT_FILE:-"mcc17.csv"}
 
 TMP=$(mktemp ./results2csv.tmp.XXXXXX)
 
