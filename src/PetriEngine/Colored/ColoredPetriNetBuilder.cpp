52522ff61108cc8c83f684ed283e81bc1911c2eb
Peter Taankvist ptaankvist@gmail.com <Peter Taankvist ptaankvist@gmail.com>
2020-11-25 09:04:35 +0100

    const makes it faster

--- src/PetriEngine/Colored/ColoredPetriNetBuilder.cpp
+++ src/PetriEngine/Colored/ColoredPetriNetBuilder.cpp
@@ -453,7 +453,7 @@ namespace PetriEngine {
             if (color.second == 0) {
                 continue;
             }
-            PetriEngine::Colored::Place& place = _places[arc.place];
+            const PetriEngine::Colored::Place& place = _places[arc.place];
             const std::string& pName = _ptplacenames[place.name][color.first->getId()];
             if (pName.empty()) {
                 
