2df857ead58c4045812f1e5e30cea4babf842c5c
Thomas Pedersen tpede16@student.aau.dk <Thomas Pedersen tpede16@student.aau.dk>
2021-04-22 14:43:03 +0200

    Parse vehicularWifi

--- src/PetriEngine/Colored/ColoredPetriNetBuilder.cpp
+++ src/PetriEngine/Colored/ColoredPetriNetBuilder.cpp
@@ -138,6 +138,21 @@ namespace PetriEngine {
     void ColoredPetriNetBuilder::sort() {
     }
 
+    //----------------------- Partitioning -----------------------//
+
+    void ColoredPetriNetBuilder::computePartition(){
+        auto partitionStart = std::chrono::high_resolution_clock::now();
+        Colored::PartitionBuilder pBuilder = _fixpointDone? Colored::PartitionBuilder(&_transitions, &_places, &_placePostTransitionMap, &_placePreTransitionMap, &_placeColorFixpoints) : Colored::PartitionBuilder(&_transitions, &_places, &_placePostTransitionMap, &_placePreTransitionMap);
+        
+        pBuilder.partitionNet();
+        //pBuilder.printPartion();
+        _partition = pBuilder.getPartition();
+        pBuilder.assignColorMap(_partition);
+        _partitionComputed = true;
+        auto partitionEnd = std::chrono::high_resolution_clock::now();
+        _partitionTimer = (std::chrono::duration_cast<std::chrono::microseconds>(partitionEnd - partitionStart).count())*0.000001;
+    }
+
     //----------------------- Color fixpoint -----------------------//
 
     void ColoredPetriNetBuilder::printPlaceTable() {
@@ -157,17 +172,7 @@ namespace PetriEngine {
         }
     }
 
-    void ColoredPetriNetBuilder::computePartition(){
-        auto partitionStart = std::chrono::high_resolution_clock::now();
-        Colored::PartitionBuilder pBuilder = Colored::PartitionBuilder(&_transitions, &_places, &_placePostTransitionMap, &_placePreTransitionMap);
-        pBuilder.partitionNet();
-        //pBuilder.printPartion();
-        _partition = pBuilder.getPartition();
-        pBuilder.assignColorMap(_partition);
-        _partitionComputed = true;
-        auto partitionEnd = std::chrono::high_resolution_clock::now();
-        _partitionTimer = (std::chrono::duration_cast<std::chrono::microseconds>(partitionEnd - partitionStart).count())*0.000001;
-    }
+
       
     void ColoredPetriNetBuilder::computePlaceColorFixpoint(uint32_t maxIntervals, uint32_t maxIntervalsReduced, int32_t timeout) {
         //Start timers for timing color fixpoint creation and max interval reduction steps
@@ -209,7 +214,7 @@ namespace PetriEngine {
         _fixpointDone = true;
         _fixPointCreationTime = (std::chrono::duration_cast<std::chrono::microseconds>(end - start).count())*0.000001;
 
-        //printPlaceTable();
+        printPlaceTable();
         _placeColorFixpoints.clear();
     }
 
