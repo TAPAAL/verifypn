4f7e39681fc2daeeded4e8cf6fecf777bdbbd665
Nikolaj Jensen Ulrik <njul@cs.aau.dk>
2021-08-20 09:17:27 +0200

    Slightly less aggressive CompareConjunction in optimization attempt

--- src/PetriEngine/Stubborn/InterestingTransitionVisitor.cpp
+++ src/PetriEngine/Stubborn/InterestingTransitionVisitor.cpp
@@ -387,11 +387,28 @@ namespace PetriEngine {
 
     void InterestingLTLTransitionVisitor::_accept(const PQL::CompareConjunction *element)
     {
+        auto neg = negated != element->isNegated();
         for (auto &c : *element) {
-            if (!_stubborn.seenPre(c._place))
-                _stubborn.presetOf(c._place, closure);
-            if (!_stubborn.seenPost(c._place))
-                _stubborn.postsetOf(c._place, closure);
+            if (!neg) {
+                if (c._lower != 0 && !_stubborn.seenPre(c._place)) {
+                    // c < p becomes satisfied by preset of p.
+                    _stubborn.presetOf(c._place, closure);
+                }
+                if (c._upper != std::numeric_limits<uint32_t>::max() && !_stubborn.seenPost(c._place)) {
+                    // p < c becomes satisfied by postset of p.
+                    _stubborn.postsetOf(c._place, closure);
+                }
+            }
+            else {
+                if (c._lower != 0 && !_stubborn.seenPost(c._place)) {
+                    // !(p < c) becomes satisfied by preset of p.
+                    _stubborn.postsetOf(c._place, closure);
+                }
+                if (c._upper != std::numeric_limits<uint32_t>::max() && !_stubborn.seenPre(c._place)) {
+                    // !(c < p) becomes satisfied by postset of p.
+                    _stubborn.presetOf(c._place, closure);
+                }
+            }
         }
     }
 
