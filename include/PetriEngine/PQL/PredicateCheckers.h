5ff4a4974678cd8be6c5d26a0236ea41558006eb
Ragusaen <rtollu18@student.aau.dk>
2021-12-16 21:13:16 +0100

    changed license

--- include/PetriEngine/PQL/PredicateCheckers.h
+++ include/PetriEngine/PQL/PredicateCheckers.h
@@ -1,6 +1,22 @@
-//
-// Created by ragusa on 28/11/2021.
-//
+/* Copyright (C) 2011  Jonas Finnemann Jensen <jopsen@gmail.com>,
+ *                     Thomas Søndersø Nielsen <primogens@gmail.com>,
+ *                     Lars Kærlund Østergaard <larsko@gmail.com>,
+ *                     Peter Gjøl Jensen <root@petergjoel.dk>
+ *                     Rasmus Tollund <rtollu18@student.aau.dk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef VERIFYPN_PREDICATECHECKERS_H
 #define VERIFYPN_PREDICATECHECKERS_H
