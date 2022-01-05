6ff5b0c6ae9155c218ec55c1cc5ee092650f5651
Ragusaen <rtollu18@student.aau.dk>
2022-01-05 11:33:12 +0100

    fixes from comments

--- boost_tests/PQLParserTests.cpp
+++ boost_tests/PQLParserTests.cpp
@@ -25,18 +25,18 @@ BOOST_AUTO_TEST_CASE(fireable_condition) {
     BOOST_ASSERT(fireable2->getName() == "t2");
 }
 
-    BOOST_AUTO_TEST_CASE(question_mark_fireable) {
-        std::string query = R"("t1"?)";
-        std::vector<std::string> _;
-        //auto expected = std::make_shared<AndCondition>(std::make_shared<FireableCondition>("t1"), std::make_shared<FireableCondition>("t2"));
+BOOST_AUTO_TEST_CASE(question_mark_fireable) {
+    std::string query = R"("t1"?)";
+    std::vector<std::string> _;
+    //auto expected = std::make_shared<AndCondition>(std::make_shared<FireableCondition>("t1"), std::make_shared<FireableCondition>("t2"));
 
-        auto actual = ParseQuery(query, _);
+    auto actual = ParseQuery(query, _);
 
-        std::shared_ptr<FireableCondition> fireable;
-        BOOST_ASSERT(fireable = std::dynamic_pointer_cast<FireableCondition>(actual));
+    std::shared_ptr<FireableCondition> fireable;
+    BOOST_ASSERT(fireable = std::dynamic_pointer_cast<FireableCondition>(actual));
 
-        BOOST_ASSERT(fireable->getName() == "t1");
-    }
+    BOOST_ASSERT(fireable->getName() == "t1");
+}
 
 BOOST_AUTO_TEST_CASE(E_deadlock) {
         std::string query = R"(E deadlock)";
@@ -50,6 +50,20 @@ BOOST_AUTO_TEST_CASE(E_deadlock) {
 
         std::shared_ptr<DeadlockCondition> deadlockCondition;
         BOOST_ASSERT(deadlockCondition = std::dynamic_pointer_cast<DeadlockCondition>((*andCondition)[0]));
+}
+
+BOOST_AUTO_TEST_CASE(E_deadlock) {
+    std::string query = R"(E deadlock)";
+    std::vector<std::string> _;
+    //auto expected = std::make_shared<AndCondition>(std::make_shared<FireableCondition>("t1"), std::make_shared<FireableCondition>("t2"));
+
+    auto actual = ParseQuery(query, _);
+
+    std::shared_ptr<ECondition> andCondition;
+    BOOST_REQUIRE(andCondition = std::dynamic_pointer_cast<ECondition>(actual));
+
+    std::shared_ptr<DeadlockCondition> deadlockCondition;
+    BOOST_ASSERT(deadlockCondition = std::dynamic_pointer_cast<DeadlockCondition>((*andCondition)[0]));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
