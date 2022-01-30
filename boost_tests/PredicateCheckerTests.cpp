840d56041e038f74f7e41b63a81f53e0df53cc1a
Ragusaen <rtollu18@student.aau.dk>
2022-01-30 20:12:07 +0100

    Fixed isReachability tests

--- boost_tests/PredicateCheckerTests.cpp
+++ boost_tests/PredicateCheckerTests.cpp
@@ -1,50 +1,42 @@
-#include <boost/test/unit_test.hpp>
 #include "PetriEngine/PQL/PQLParser.h"
 #include "PetriEngine/PQL/Expressions.h"
 #include "PetriEngine/PQL/PredicateCheckers.h"
 
+#define BOOST_TEST_MODULE PredicateCheckerTests
+#include <boost/test/included/unit_test.hpp>
+
 using namespace PetriEngine::PQL;
 
-BOOST_AUTO_TEST_SUITE(IsReachabilityTests)
-
 BOOST_AUTO_TEST_CASE(not_A_G_deadlock_true) {
-    std::vector<std::string> _;
-    auto cond = ParseQuery(R"(! A G (deadlock))", _);
+    auto cond = ParseQuery(R"(! A G (deadlock))");
 
     BOOST_TEST(isReachability(cond));
 }
 
 BOOST_AUTO_TEST_CASE(E_F_compare_true) {
-    std::vector<std::string> _;
-    auto cond = ParseQuery(R"(E F ("p1" < 4))", _);
+    auto cond = ParseQuery(R"(E F ("p1" < 4))");
     bool actual = isReachability(cond);
 
     BOOST_TEST(actual);
 }
 
 BOOST_AUTO_TEST_CASE(AG_deadlock_or_EF_deadlock_false) {
-    std::vector<std::string> _;
-    auto cond = ParseQuery(R"(A G (deadlock) || E F (deadlock))", _);
+    auto cond = ParseQuery(R"(A G (deadlock) || E F (deadlock))");
     bool actual = isReachability(cond);
 
     BOOST_TEST(!actual);
 }
 
 BOOST_AUTO_TEST_CASE(EF_AG_deadlock_or_deadlock_false) {
-    std::vector<std::string> _;
-    auto cond = ParseQuery(R"(E F (A G (deadlock) || (deadlock)) )", _);
+    auto cond = ParseQuery(R"(E F (A G (deadlock) || (deadlock)) )");
     bool actual = isReachability(cond);
 
     BOOST_TEST(!actual);
 }
 
 BOOST_AUTO_TEST_CASE(deadlock_false) {
-    std::vector<std::string> _;
-    auto cond = ParseQuery(R"(deadlock)", _);
+    auto cond = ParseQuery(R"(deadlock)");
     bool actual = isReachability(cond);
 
     BOOST_TEST(!actual);
 }
-
-
-BOOST_AUTO_TEST_SUITE_END()
