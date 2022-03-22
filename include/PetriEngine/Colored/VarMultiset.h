c341c40fb419b1c520169626316f89cb962a600f
NicEastvillage <nicoesterby@gmail.com>
2022-03-22 10:49:27 +0100

    Add toString, isSubsetOf, and iterator

--- include/PetriEngine/Colored/VarMultiset.h
+++ include/PetriEngine/Colored/VarMultiset.h
@@ -15,29 +15,50 @@
 namespace PetriEngine::Colored {
     class VarMultiset {
     private:
-        typedef std::vector<std::pair<const Variable*, uint32_t>> Internal;
+        class Iterator;
+
+        typedef std::vector<std::pair<const Variable *, uint32_t>> Internal;
 
         Internal _set;
-        const ColorType* _type;
+        const ColorType *_type;
     public:
         VarMultiset() : _set(), _type(nullptr) {};
-        VarMultiset(const ColorType* type) : _set(), _type(type) {};
-        VarMultiset(const VarMultiset&) = default;
-        VarMultiset(VarMultiset&&) = default;
-        VarMultiset(const Variable*, uint32_t);
+
+        VarMultiset(const ColorType *type) : _set(), _type(type) {};
+
+        VarMultiset(const VarMultiset &) = default;
+
+        VarMultiset(VarMultiset &&) = default;
+
+        VarMultiset(const Variable *, uint32_t);
+
         ~VarMultiset() = default;
 
-        VarMultiset& operator=(const VarMultiset&) = default;
-        VarMultiset& operator=(VarMultiset&&) = default;
+        Iterator begin() const;
 
-        VarMultiset operator+ (const VarMultiset& other) const;
-        VarMultiset operator- (const VarMultiset& other) const;
-        VarMultiset operator* (uint32_t scalar) const;
-        void operator+= (const VarMultiset& other);
-        void operator-= (const VarMultiset& other);
-        void operator*= (uint32_t scalar);
-        uint32_t operator[] (const Variable* color) const;
-        uint32_t& operator[] (const Variable* color);
+        Iterator end() const;
+
+        VarMultiset &operator=(const VarMultiset &) = default;
+
+        VarMultiset &operator=(VarMultiset &&) = default;
+
+        VarMultiset operator+(const VarMultiset &other) const;
+
+        VarMultiset operator-(const VarMultiset &other) const;
+
+        VarMultiset operator*(uint32_t scalar) const;
+
+        void operator+=(const VarMultiset &other);
+
+        void operator-=(const VarMultiset &other);
+
+        void operator*=(uint32_t scalar);
+
+        uint32_t operator[](const Variable *color) const;
+
+        uint32_t &operator[](const Variable *color);
+
+        bool isSubsetOf(const VarMultiset &other) const;
 
         bool empty() const {
             return size() == 0;
@@ -48,6 +69,29 @@ namespace PetriEngine::Colored {
         size_t distinctSize() const {
             return _set.size();
         }
+
+        std::string toString() const;
+
+    private:
+        class Iterator {
+        private:
+            const VarMultiset *_ms;
+            size_t _index;
+
+        public:
+            Iterator(const VarMultiset *ms, size_t index)
+                    : _ms(ms), _index(index) {}
+
+            bool operator==(Iterator &other);
+
+            bool operator!=(Iterator &other);
+
+            Iterator &operator++();
+
+            std::pair<const Variable *, const uint32_t &> operator++(int);
+
+            std::pair<const Variable *, const uint32_t &> operator*();
+        };
     };
 }
 
--- src/PetriEngine/Colored/VarMultiset.cpp
+++ src/PetriEngine/Colored/VarMultiset.cpp
@@ -8,23 +8,32 @@
 #include "PetriEngine/Colored/VarMultiset.h"
 
 namespace PetriEngine::Colored {
-    VarMultiset::VarMultiset(const PetriEngine::Colored::Variable * v, const uint32_t multiplicity) : _set(), _type(v->colorType) {
+    VarMultiset::VarMultiset(const PetriEngine::Colored::Variable *v, const uint32_t multiplicity)
+            : _set(), _type(v->colorType) {
         _set.emplace_back(v, multiplicity);
     }
 
-    VarMultiset VarMultiset::operator +(const VarMultiset& other) const {
+    VarMultiset::Iterator VarMultiset::begin() const {
+        return Iterator(this, 0);
+    }
+
+    VarMultiset::Iterator VarMultiset::end() const {
+        return Iterator(this, _set.size());
+    }
+
+    VarMultiset VarMultiset::operator+(const VarMultiset &other) const {
         VarMultiset ms(*this);
         ms += other;
         return ms;
     }
 
-    VarMultiset VarMultiset::operator -(const VarMultiset& other) const {
+    VarMultiset VarMultiset::operator-(const VarMultiset &other) const {
         VarMultiset ms(*this);
         ms -= other;
         return ms;
     }
 
-    VarMultiset VarMultiset::operator *(uint32_t scalar) const {
+    VarMultiset VarMultiset::operator*(uint32_t scalar) const {
         VarMultiset ms(*this);
         ms *= scalar;
         return ms;
@@ -34,7 +43,7 @@ namespace PetriEngine::Colored {
         if (_type == nullptr) _type = other._type;
         if (_type != other._type)
             throw base_error("You cannot compare variable multisets of different types");
-        for (auto pair : other._set) {
+        for (auto pair: other._set) {
             (*this)[pair.first] += pair.second;
         }
     }
@@ -43,32 +52,32 @@ namespace PetriEngine::Colored {
         if (_type == nullptr) _type = other._type;
         if (other._type != nullptr && _type != other._type)
             throw base_error("You cannot add multisets over different sets");
-        for (auto pair : _set) {
+        for (auto pair: _set) {
             (*this)[pair.first] = std::min<uint32_t>(pair.second - other[pair.first], 0); // min because underflow
         }
     }
 
     void VarMultiset::operator*=(uint32_t scalar) {
-        for (auto& pair : _set) {
+        for (auto &pair: _set) {
             pair.second *= scalar;
         }
     }
 
-    uint32_t VarMultiset::operator [](const Variable* var) const {
+    uint32_t VarMultiset::operator[](const Variable *var) const {
         if (_type == nullptr || _type == var->colorType) {
-            for (auto pair : _set) {
+            for (auto pair: _set) {
                 if (pair.first == var) return pair.second;
             }
         }
         return 0;
     }
 
-    uint32_t& VarMultiset::operator [](const Variable* var) {
+    uint32_t &VarMultiset::operator[](const Variable *var) {
         if (_type == nullptr) _type = var->colorType;
         if (_type != var->colorType) {
             throw base_error("You cannot access a multiset with a variable from a different color type");
         }
-        for (auto & i : _set) {
+        for (auto &i: _set) {
             if (i.first == var) return i.second;
         }
         _set.emplace_back(var, 0);
@@ -77,9 +86,45 @@ namespace PetriEngine::Colored {
 
     size_t VarMultiset::size() const {
         size_t res = 0;
-        for (auto pair : _set) {
+        for (auto pair: _set) {
             res += pair.second;
         }
         return res;
+    }
+
+    bool VarMultiset::isSubsetOf(const VarMultiset &other) const {
+        VarMultiset thisMinusOther(*this);
+        thisMinusOther -= other;
+        VarMultiset otherMinusThis(other);
+        otherMinusThis -= *this;
+        return thisMinusOther.empty() && !otherMinusThis.empty();
+    }
+
+    std::string VarMultiset::toString() const {
+        std::ostringstream oss;
+        for (size_t i = 0; i < _set.size(); ++i) {
+            oss << _set[i].second << "'" << _set[i].first->name;
+            if (i < _set.size() - 1) {
+                oss << " + ";
+            }
+        }
+        return oss.str();
+    }
+
+    bool VarMultiset::Iterator::operator==(VarMultiset::Iterator &other) {
+        return _ms == other._ms && _index == other._index;
+    }
+
+    bool VarMultiset::Iterator::operator!=(VarMultiset::Iterator &other) {
+        return !(*this == other);
+    }
+
+    VarMultiset::Iterator &VarMultiset::Iterator::operator++() {
+        ++_index;
+        return *this;
+    }
+
+    std::pair<const Variable *, const uint32_t &> VarMultiset::Iterator::operator*() {
+        return _ms->_set[_index];
     }
 }
