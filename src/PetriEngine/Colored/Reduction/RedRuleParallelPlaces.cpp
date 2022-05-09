0035dee1ea082c434dcb2ae0f7f3b6b0e72b4525
NicEastvillage <nicoesterby@gmail.com>
2022-05-09 11:37:11 +0200

    Update Rule C (#63)
    * Update ParP rule to new definition
    
    * Fix bug and remove extra constraints
    
    * Ensure places have same type
    
    * Add extra hasTimedOut call

--- src/PetriEngine/Colored/Reduction/RedRuleParallelPlaces.cpp
+++ src/PetriEngine/Colored/Reduction/RedRuleParallelPlaces.cpp
@@ -13,147 +13,178 @@ namespace PetriEngine::Colored::Reduction {
     bool RedRuleParallelPlaces::apply(ColoredReducer &red, const PetriEngine::PQL::ColoredUseVisitor &inQuery,
                                       QueryType queryType, bool preserveLoops, bool preserveStutter) {
 
-        // Remove places which input and output is k times another place's input and output
+        // Remove places in parallel where one accumulates tokens while the other disable their post set
 
         bool continueReductions = false;
 
-        auto &places = red.places();
-        auto &transitions = red.transitions();
-
-        red._pflags.resize(places.size());
+        red._pflags.resize(red.placeCount(), 0);
         std::fill(red._pflags.begin(), red._pflags.end(), 0);
 
-        for (uint32_t touter = 0; touter < transitions.size(); touter++) {
-            for (int outer = 0; outer < transitions[touter].output_arcs.size(); outer++) {
+        for (uint32_t tid_outer = 0; tid_outer < red.transitionCount(); ++tid_outer) {
+            for (size_t aid_outer = 0; aid_outer < red.transitions()[tid_outer].output_arcs.size(); ++aid_outer) {
 
-                auto pouter = transitions[touter].output_arcs[outer].place;
-                if (red._pflags[pouter] > 0) continue;
-                red._pflags[pouter] = 1;
+                auto pid_outer = red.transitions()[tid_outer].output_arcs[aid_outer].place;
+                if (red._pflags[pid_outer] > 0) continue;
+                red._pflags[pid_outer] = 1;
+
                 if (red.hasTimedOut()) return false;
 
-                const Place &pout = places[pouter];
-
+                const Place &pout = red.places()[pid_outer];
                 if (pout.skipped) continue;
 
-                for (uint32_t inner = outer + 1; inner < transitions[touter].output_arcs.size(); inner++) {
+                for (size_t aid_inner = aid_outer + 1; aid_inner < red.transitions()[tid_outer].output_arcs.size(); ++aid_inner) {
                     if (pout.skipped) break;
+                    auto pid_inner = red.transitions()[tid_outer].output_arcs[aid_inner].place;
+                    if (red.places()[pid_inner].skipped) continue;
 
-                    auto pinner = transitions[touter].output_arcs[inner].place;
-                    if (places[pinner].skipped) continue;
+                    if (red.places()[pid_inner].type != red.places()[pid_outer].type) continue;
 
-                    if (pouter == pinner) continue;
-                    if (places[pinner].type != places[pouter].type) continue;
-
-                    for (size_t swp = 0; swp < 2; swp++) {
-
+                    for (size_t swp = 0; swp < 2; ++swp) {
                         if (red.hasTimedOut()) return false;
-                        if (places[pinner].skipped || places[pouter].skipped) break;
+                        if (red.places()[pid_inner].skipped ||
+                            red.places()[pid_outer].skipped)
+                            break;
 
-                        uint32_t p1 = pouter;
-                        uint32_t p2 = pinner;
+                        uint p1 = pid_outer;
+                        uint p2 = pid_inner;
+
+                        assert(p1 != p2);
                         if (swp == 1) std::swap(p1, p2);
-
-                        // We will now check if p2 can be removed
 
                         if (inQuery.isPlaceUsed(p2)) continue;
 
-                        const Place &place1 = places[p1];
-                        const Place &place2 = places[p2];
+                        const Place &place1 = red.places()[p1];
+                        const Place &place2 = red.places()[p2];
 
-                        if (place1._pre.size() > place2._pre.size() ||
-                            place1._post.size() < place2._post.size())
+                        if (place2._pre.empty() || place1._post.empty()) continue;
+
+                        if (place1._post.size() < place2._post.size() ||
+                            place1._pre.size() > place2._pre.size())
                             continue;
 
-                        if (place2.inhibitor) continue; // TODO can be generalized, also with k scaling
-
-                        double mult = 1.0;
-
-                        int fail = 0;
-                        size_t j = 0;
-                        for (size_t i = 0; i < place2._post.size(); i++) {
-
-                            // place1 may have consumers that place2 does not
-                            while (j < place1._post.size() && place1._post[j] < place2._post[i]) j++;
-                            if (place1._post.size() <= j || place1._post[j] != place2._post[i]) {
-                                fail = 2;
-                                break;
-                            }
-
-                            const Transition &trans = transitions[place1._post[j]];
-                            auto a1 = red.getInArc(p1, trans);
-                            auto a2 = red.getInArc(p2, trans);
-                            assert(a1 != trans.input_arcs.end());
-                            assert(a2 != trans.input_arcs.end());
-
-                            if (to_string(*a1->expr) == to_string(*a2->expr)) {
-                                continue; // mult is already at least 1
-                            } else if (auto ms1 = PetriEngine::Colored::extractVarMultiset(*a1->expr)) {
-                                if (auto ms2 = PetriEngine::Colored::extractVarMultiset(*a2->expr)) {
-                                    if (auto k = ms1->scaleRequiredToCover(*ms2)) {
-                                        mult = std::max(mult, *k);
-                                        continue;
-                                    }
-                                }
-                            }
-                            fail = 2;
+                        // Initial marking must share support
+                        if (place1.marking.distinctSize() != place2.marking.distinctSize()
+                                || (place1.marking + place2.marking).distinctSize() != place1.marking.distinctSize())
                             break;
-                        }
 
-                        if (fail == 2) break;
+                        bool ok = true;
 
-                        // TODO Could be more precise with fuzzy multisets
-                        if (!(place1.marking * (uint32_t)std::ceil(mult)).isSubsetOrEqTo(place2.marking)) continue;
+                        double maxDrainRatio = 0;
 
-                        j = 0;
-                        for (size_t i = 0; i < place1._pre.size(); i++) {
+                        uint32_t i = 0, j = 0;
+                        while (i < place1._post.size() && j < place2._post.size()) {
 
-                            // place2 may have producers that place1 does not
-                            while (j < place2._pre.size() && place2._pre[j] < place1._pre[i]) j++;
-                            if (j == place2._pre.size() || place1._pre[j] != place2._pre[i]) {
-                                fail = 2;
+                            uint32_t p1t = place1._post[i];
+                            uint32_t p2t = place2._post[j];
+
+                            if (p2t < p1t) {
+                                // place2._post is not a subset of place1._post
+                                ok = false;
                                 break;
                             }
 
-                            const Transition &trans = transitions[place1._pre[j]];
-                            auto a1 = red.getOutArc(trans, p1);
-                            auto a2 = red.getOutArc(trans, p2);
-                            assert(a1 != trans.output_arcs.end());
-                            assert(a2 != trans.output_arcs.end());
-
-                            if (mult == 1.0 && to_string(*a1->expr) == to_string(*a2->expr)) {
+                            i++;
+                            if (p2t > p1t) {
+                                swp = 2; // We can't remove p1, so don't try swap
                                 continue;
-                            } else if (auto ms1 = PetriEngine::Colored::extractVarMultiset(*a1->expr)) {
-                                if (auto ms2 = PetriEngine::Colored::extractVarMultiset(*a2->expr)) {
-                                    // TODO Could be more precise with fuzzy multisets
-                                    if ((*ms1 * (uint32_t)std::ceil(mult)).isSubsetOrEqTo(*ms2)) {
-                                        continue;
-                                    }
+                            }
+                            j++;
+
+                            const Transition &tran = red.transitions()[p1t];
+                            const auto &p1Arc = red.getInArc(p1, tran);
+                            const auto &p2Arc = red.getInArc(p2, tran);
+
+                            if (to_string(*p1Arc->expr) == to_string(*p2Arc->expr)) {
+                                maxDrainRatio = std::max(maxDrainRatio, 1.0);
+                                continue;
+                            }
+
+                            const auto ms1 = PetriEngine::Colored::extractVarMultiset(*p1Arc->expr);
+                            const auto ms2 = PetriEngine::Colored::extractVarMultiset(*p2Arc->expr);
+
+                            // ms1 and ms2 must share support
+
+                            if (!ms1 || !ms2 || ms1->distinctSize() != ms2->distinctSize() || ((*ms1) + (*ms2)).distinctSize() != ms1->distinctSize()) {
+                                ok = false;
+                                swp = 2;
+                                break;
+                            }
+
+                            for (const auto& [varvec, multiplicity] : *ms1) {
+                                maxDrainRatio = std::max(maxDrainRatio, (double)(*ms2)[varvec] / (double)multiplicity);
+                            }
+                        }
+
+                        if (!ok || j != place2._post.size()) continue;
+
+                        if (!(place1.marking * maxDrainRatio).isSubsetOrEqTo(place2.marking)) continue;
+
+                        i = 0, j = 0;
+                        while (i < place1._pre.size() && j < place2._pre.size()) {
+                            if (red.hasTimedOut()) return false;
+
+                            uint32_t p1t = place1._pre[i];
+                            uint32_t p2t = place2._pre[j];
+
+                            if (p1t < p2t) {
+                                // place1._pre is not a subset of place2._pre
+                                ok = false;
+                                break;
+                            }
+
+                            j++;
+                            if (p1t > p2t) {
+                                swp = 2; // We can't remove p1, so don't try swap
+                                continue;
+                            }
+                            i++;
+
+                            const Transition &tran = red.transitions()[p2t];
+                            const auto &p2Arc = red.getOutArc(tran, p2);
+                            const auto &p1Arc = red.getOutArc(tran, p1);
+
+                            if (to_string(*p1Arc->expr) == to_string(*p2Arc->expr) && maxDrainRatio > 1.0) {
+                                ok = false;
+                                break;
+                            }
+
+                            const auto ms1 = PetriEngine::Colored::extractVarMultiset(*p1Arc->expr);
+                            const auto ms2 = PetriEngine::Colored::extractVarMultiset(*p2Arc->expr);
+
+                            // ms1 and ms2 must share support
+
+                            if (!ms1 || !ms2 || ms1->distinctSize() != ms2->distinctSize() || ((*ms1) + (*ms2)).distinctSize() != ms1->distinctSize()) {
+                                ok = false;
+                                swp = 2;
+                                break;
+                            }
+
+                            for (const auto& [varvec, multiplicity] : *ms1) {
+                                if (maxDrainRatio > (double)(*ms2)[varvec] / (double)multiplicity) {
+                                    ok = false;
+                                    break;
                                 }
                             }
-                            fail = 1;
-                            break;
+                            if (!ok) break;
                         }
 
-                        if (fail == 2) break;
-                        else if (fail == 1) continue;
+                        if (!ok || i != place1._pre.size()) continue;
 
-                        // Remove p2
-                        _applications++;
                         continueReductions = true;
+                        _applications++;
                         red.skipPlace(p2);
-                        red._pflags[pouter] = 0;
-                        // touter.output_arcs have shrunk, so go one back to not miss any
-                        if (p2 == pouter) {
-                            outer--;
-                            inner--;
-                        }
-                        else if (p2 == pinner) inner--;
+
+                        // p2 has now been removed from tid_outer.output_arcs, so update arc indexes to not miss any places
+                        if (p2 == pid_outer) {
+                            aid_outer--;
+                            aid_inner--;
+                        } else if (p2 == pid_inner) aid_inner--;
                         break;
                     }
                 }
             }
         }
-
+        red.consistent();
         return continueReductions;
     }
 }
