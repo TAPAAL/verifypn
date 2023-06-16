4fbc0098ca90954f58572d23a94ac99f6cbd4403
pgj <root@petergjoel.dk>
2023-06-16 11:51:18 +0200

    headers for picky compilers

--- include/PetriEngine/options.h
+++ include/PetriEngine/options.h
@@ -1,6 +1,9 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
 
+#include "CTL/Algorithm/AlgorithmTypes.h"
+#include "LTL/LTLOptions.h"
+
 #include <ctype.h>
 #include <stddef.h>
 #include <limits>
@@ -8,9 +11,8 @@
 #include <sstream>
 #include <vector>
 #include <iostream>
+#include <cstdint>
 
-#include "CTL/Algorithm/AlgorithmTypes.h"
-#include "LTL/LTLOptions.h"
 
 enum class Strategy {
     BFS,
