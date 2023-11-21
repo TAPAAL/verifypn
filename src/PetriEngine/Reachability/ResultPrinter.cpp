d32976bcee4271905b3c2f0d2ef5db41a16301e7
Jiri Srba <srba.jiri@gmail.com>
2023-11-21 12:01:02 +0100

    Fixing problem with printing traces where reduction was disabled.

--- src/PetriEngine/Reachability/ResultPrinter.cpp
+++ src/PetriEngine/Reachability/ResultPrinter.cpp
@@ -214,6 +214,8 @@ namespace PetriEngine {
                 transitions.push(p.second);
             }
 
+            auto reducer = builder->getReducer();
+            
             if(reducer != nullptr)
                 reducer->initFire(std::cerr);
 
