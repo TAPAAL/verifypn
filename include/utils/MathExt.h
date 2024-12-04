2afc63b7b8dcc9c076a037032433af455a85c924
Jens Emil Fink Højriis <5774453+JE-FH@users.noreply.github.com>
2024-12-04 11:05:29 +0100

    documented function better

--- include/utils/MathExt.h
+++ include/utils/MathExt.h
@@ -1,7 +1,8 @@
 #ifndef MATHEXT_H
 #define MATHEXT_H
 
-//
+/// maps value to [0,max) using modulo but when value is negative
+/// it is handled differently s.t. signed_wrap(-5, 5) = 4
 template <typename S, typename U>
 U signed_wrap(S value, U max) {
     return ((value % max) + max) % max;
