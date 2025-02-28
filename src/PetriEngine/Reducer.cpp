d61512e3554cc6b5d1ab6a11acad111c548c6c52
srba <srba@users.noreply.github.com>
2025-02-28 20:46:27 +0100

    Update Reducer.cpp

--- src/PetriEngine/Reducer.cpp
+++ src/PetriEngine/Reducer.cpp
@@ -1595,7 +1595,7 @@ namespace PetriEngine {
             for(auto& t : parent->_places[p].producers)
             {
                 auto& trans = getTransition(t);
-                auto arc = getInArc(p, trans);
+                auto arc = getOutArc(p, trans);
                 arc->weight /= mod;
             }
             parent->initialMarking[p] /= mod;
