4a78be55a1ba32278e368e32b3037bee4ab8d656
Jens Emil Fink Højriis <5774453+JE-FH@users.noreply.github.com>
2025-06-07 23:11:04 +0200

    improved interactive mode to print current marking and allow multiple transitions

--- include/PetriEngine/ExplicitColored/ExplicitColoredInteractiveMode.h
+++ include/PetriEngine/ExplicitColored/ExplicitColoredInteractiveMode.h
@@ -5,13 +5,24 @@
 
 #include "ColoredPetriNetMarking.h"
 #include "ExplicitColoredPetriNetBuilder.h"
+#include "SuccessorGenerator/ColoredSuccessorGenerator.h"
 
 namespace PetriEngine::ExplicitColored {
     class ExplicitColoredInteractiveMode {
     public:
         static int run(const std::string& model_path);
     private:
-        static std::optional<ColoredPetriNetMarking> parseMarking(const ExplicitColoredPetriNetBuilder& builder, const ColoredPetriNet& cpn, std::string markingXml, std::ostream& errorOut);
+        ExplicitColoredInteractiveMode(const ColoredSuccessorGenerator& successorGenerator, const ColoredPetriNet& cpn, const ExplicitColoredPetriNetBuilder& builder);
+        int _run_internal();
+        static std::string _readUntilDoubleNewline(std::istream& in);
+        std::optional<ColoredPetriNetMarking> _parseMarking(const rapidxml::xml_document<>& markingXml, std::ostream& errorOut) const;
+        std::optional<std::pair<Transition_t, Binding>> _parseTransition(const rapidxml::xml_document<>& transitionXml, std::ostream& errorOut) const;
+        void _printCurrentMarking(std::ostream& out, const ColoredPetriNetMarking& currentMarking) const;
+        void _printValidBindings(std::ostream& out, const ColoredPetriNetMarking& currentMarking) const;
+        std::optional<Color_t> _findColorIndex(const Colored::ColorType* colorType, const char* colorName) const;
+        const ColoredSuccessorGenerator& _successorGenerator;
+        const ColoredPetriNet& _cpn;
+        const ExplicitColoredPetriNetBuilder& _builder;
     };
 }
 #endif //COLOREDINTERACTIVEMODE_H
--- src/PetriEngine/ExplicitColored/ExplicitColoredInteractiveMode.cpp
+++ src/PetriEngine/ExplicitColored/ExplicitColoredInteractiveMode.cpp
@@ -24,62 +24,145 @@ namespace PetriEngine::ExplicitColored {
         }
 
         auto cpn = builder.takeNet();
+        ColoredSuccessorGenerator successorGenerator(cpn);
 
-        std::cin >> std::noskipws;
+        ExplicitColoredInteractiveMode self(successorGenerator, cpn, builder);
+        return self._run_internal();
+    }
 
-        std::istream_iterator<char> it(std::cin);
-        std::istream_iterator<char> end;
-        std::string markingXml(it, end);
+    ExplicitColoredInteractiveMode::ExplicitColoredInteractiveMode(
+        const ColoredSuccessorGenerator& successorGenerator,
+        const ColoredPetriNet& cpn,
+        const ExplicitColoredPetriNetBuilder& builder
+    )
+    : _successorGenerator(successorGenerator), _cpn(cpn), _builder(builder) { }
 
-        const auto markingOption = parseMarking(builder, cpn, markingXml, std::cerr);
-        if (!markingOption.has_value()) {
+    int ExplicitColoredInteractiveMode::_run_internal() {
+        auto initialMarkingInput = _readUntilDoubleNewline(std::cin);
+
+        rapidxml::xml_document<> initialMarkingXml;
+        initialMarkingXml.parse<0>(initialMarkingInput.data());
+
+        auto initialMarkingOption = _parseMarking(initialMarkingXml, std::cerr);
+
+        if (!initialMarkingOption.has_value()) {
             return 1;
         }
-        const auto& marking = markingOption.value();
 
-        ColoredSuccessorGenerator successorGenerator(cpn);
-        std::cout << "<valid-bindings>" << std::endl;
-        for (Transition_t transition = 0; transition < cpn.getTransitionCount(); transition++) {
+        auto marking = std::move(*initialMarkingOption);
+
+        _printValidBindings(std::cout, marking);
+        std::cout << std::endl << std::endl;
+
+        while (true) {
+            auto input = _readUntilDoubleNewline(std::cin);
+
+            rapidxml::xml_document<> inputXml;
+            inputXml.parse<0>(input.data());
+
+            if (inputXml.first_node() == nullptr) {
+                std::cerr << "No xml was given" << std::endl;
+            }
+
+            if (inputXml.first_node()->name() == std::string("marking")) {
+                auto markingOption = _parseMarking(initialMarkingXml, std::cerr);
+
+                if (!markingOption.has_value()) {
+                    return 1;
+                }
+
+                marking = std::move(*markingOption);
+            } else {
+                auto selectedTransition = _parseTransition(inputXml, std::cerr);
+
+                if (!selectedTransition.has_value()) {
+                    return 1;
+                }
+
+                _successorGenerator.fire(marking, selectedTransition->first, selectedTransition->second);
+
+                _printCurrentMarking(std::cout, marking);
+                std::cout << std::endl << std::endl;
+            }
+            _printValidBindings(std::cout, marking);
+            std::cout << std::endl << std::endl;
+        }
+        return 0;
+    }
+
+    std::string ExplicitColoredInteractiveMode::_readUntilDoubleNewline(std::istream& in) {
+        in >> std::noskipws;
+        std::stringstream rv;
+        std::string line;
+        bool lastIsNewline = false;
+        while (std::getline(in, line)) {
+            if (line.empty()) {
+                if (lastIsNewline) {
+                    return rv.str();
+                }
+                lastIsNewline = true;
+            } else {
+                lastIsNewline = false;
+            }
+            rv << line;
+        }
+        return rv.str();
+    }
+
+    void ExplicitColoredInteractiveMode::_printValidBindings(std::ostream &out, const ColoredPetriNetMarking &currentMarking) const {
+        out << "<valid-bindings>" << std::endl;
+        for (Transition_t transition = 0; transition < _cpn.getTransitionCount(); transition++) {
             Binding_t currentBinding = 0;
             bool first = true;
             while (true) {
                 Binding actualBinding = {};
-                currentBinding = successorGenerator.findNextValidBinding(marking, transition, currentBinding, cpn.getTotalBindings(transition), actualBinding, 0);
+                currentBinding = _successorGenerator.findNextValidBinding(currentMarking, transition, currentBinding, _cpn.getTotalBindings(transition), actualBinding, 0);
                 if (currentBinding == std::numeric_limits<Binding_t>::max()) {
                     break;
                 }
                 if (first) {
-                    std::cout << "\t<transition id=" << std::quoted(builder.getPlaceName(transition)) << ">" << std::endl;
+                    out << "\t<transition id=" << std::quoted(_builder.getTransitionName(transition)) << ">" << std::endl;
                     first = false;
                 }
-                std::cout << "\t\t<binding>" << std::endl;
+                out << "\t\t<binding>" << std::endl;
                 for (const auto& [key, val] : actualBinding.getValues()) {
-                    std::cout << "\t\t\t<variable id=" << std::quoted(builder.getVariableName(key)) << ">" << std::endl;
-                    const auto& colorType = *builder.getUnderlyingVariableColorTypes().at(key);
+                    out << "\t\t\t<variable id=" << std::quoted(_builder.getVariableName(key)) << ">" << std::endl;
+                    const auto& colorType = *_builder.getUnderlyingVariableColorTypes().at(key);
 
                     const auto& colorId = colorType[val];
-                    std::cout << "\t\t\t\t<color id=" << std::quoted(colorId.getColorName()) << "/>" << std::endl;
-                    std::cout << "\t\t\t</variable>" << std::endl;
+                    out << "\t\t\t\t<color>" << colorId.getColorName() << "</color>" << std::endl;
+                    out << "\t\t\t</variable>" << std::endl;
                 }
-                std::cout << "\t\t</binding>" << std::endl;
+                out << "\t\t</binding>" << std::endl;
                 currentBinding++;
             }
             if (!first) {
-                std::cout << "\t</transition>" << std::endl;
+                out << "\t</transition>" << std::endl;
             }
         }
-        std::cout << "</valid-bindings>" << std::endl;
-
-        return 0;
+        out << "</valid-bindings>" << std::endl;
     }
 
-    std::optional<ColoredPetriNetMarking> ExplicitColoredInteractiveMode::parseMarking(const ExplicitColoredPetriNetBuilder& builder, const ColoredPetriNet& cpn, std::string markingXml, std::ostream& errorOut) {
-        rapidxml::xml_document<> doc;
-        doc.parse<0>(markingXml.data());
-        const auto root = doc.first_node();
+    std::optional<Color_t> ExplicitColoredInteractiveMode::_findColorIndex(
+        const Colored::ColorType* colorType,
+        const char *colorName
+    ) const {
+        for (const auto& color : *colorType) {
+            if (color.getColorName() == colorName) {
+                return color.getId();
+            }
+        }
+        return std::nullopt;
+    }
+
+    std::optional<ColoredPetriNetMarking> ExplicitColoredInteractiveMode::_parseMarking(
+        const rapidxml::xml_document<>& markingXml,
+        std::ostream& errorOut
+    ) const {
+        const auto root = markingXml.first_node();
 
         ColoredPetriNetMarking generatedMarking;
-        generatedMarking.markings.resize(builder.getPlaceCount());
+        generatedMarking.markings.resize(_builder.getPlaceCount());
 
         auto placeNode = root->first_node();
         if (root->name() != std::string("marking")) {
@@ -97,13 +180,13 @@ namespace PetriEngine::ExplicitColored {
                 return std::nullopt;
             }
 
-            auto markingPlaceIndexIt = builder.getPlaceIndices().find(placeNode->first_attribute("id")->value());
-            if (markingPlaceIndexIt == builder.getPlaceIndices().end()) {
+            auto markingPlaceIndexIt = _builder.getPlaceIndices().find(placeNode->first_attribute("id")->value());
+            if (markingPlaceIndexIt == _builder.getPlaceIndices().end()) {
                 errorOut << "Unknown place id " << std::quoted(placeNode->first_attribute("id")->value()) << std::endl;
                 return std::nullopt;
             }
             auto tokenNode = placeNode->first_node();
-            auto placeColorType = builder.getPlaceUnderlyingColorType(markingPlaceIndexIt->second);
+            auto placeColorType = _builder.getPlaceUnderlyingColorType(markingPlaceIndexIt->second);
             do {
                 if (tokenNode->name() != std::string("token")) {
                     errorOut << "Unexpected tag " << std::quoted(tokenNode->name()) << std::endl;
@@ -125,7 +208,7 @@ namespace PetriEngine::ExplicitColored {
                 }
                 auto colorComponentNode = tokenNode->first_node();
                 uint64_t encodedColor = 0;
-                const auto& colorCodec = cpn.getPlaces()[markingPlaceIndexIt->second].colorType->colorCodec;
+                const auto& colorCodec = _cpn.getPlaces()[markingPlaceIndexIt->second].colorType->colorCodec;
                 auto componentIndex = 0;
                 do {
                     if (colorComponentNode->name() != std::string("color")) {
@@ -141,22 +224,132 @@ namespace PetriEngine::ExplicitColored {
                     if (auto productColor = dynamic_cast<const Colored::ProductType*>(placeColorType)) {
                         currentColorType = productColor->getNestedColorType(componentIndex);
                     }
-                    bool found = false;
-                    for (const auto& color : *currentColorType) {
-                        if (color.getColorName() == colorName) {
-                            encodedColor = colorCodec.addToValue(encodedColor, componentIndex, color.getId());
-                            found = true;
-                            break;
-                        }
-                    }
-                    if (found == false) {
+
+                    auto colorIndex = _findColorIndex(currentColorType, colorName);
+                    if (colorIndex == std::nullopt) {
                         errorOut << "Unknown color id " << std::quoted(colorName) << std::endl;
                         return std::nullopt;
                     }
+                    encodedColor = colorCodec.addToValue(encodedColor, componentIndex, *colorIndex);
+
                     componentIndex++;
                 } while ((colorComponentNode = colorComponentNode->next_sibling()) != nullptr);
                 generatedMarking.markings[markingPlaceIndexIt->second].addCount(encodedColor, count);
             } while ((tokenNode = tokenNode->next_sibling()) != nullptr);
         } while ((placeNode = placeNode->next_sibling()) != nullptr);
+
+        return generatedMarking;
+    }
+
+    std::optional<std::pair<Transition_t, Binding>> ExplicitColoredInteractiveMode::_parseTransition(
+        const rapidxml::xml_document<>& transitionXml,
+        std::ostream &errorOut
+    ) const {
+        const auto transitionNode = transitionXml.first_node();
+        if (transitionNode->name() != std::string("transition")) {
+            errorOut << "Unexpected tag " << std::quoted(transitionNode->name()) << std::endl;
+            return std::nullopt;
+        }
+        if (transitionNode->first_attribute("id") == nullptr) {
+            errorOut << "Missing id attribute on transition tag" << std::endl;
+            return std::nullopt;
+        }
+        const auto transitionId = transitionNode->first_attribute("id")->value();
+        const auto transitionIndexIt = _builder.getTransitionIndices().find(transitionId);
+        if (transitionIndexIt == _builder.getTransitionIndices().end()) {
+            errorOut << "Unknown transition id "<< std::quoted(transitionId) << std::endl;
+            return std::nullopt;
+        }
+
+        const auto bindingNode = transitionNode->first_node();
+        if (bindingNode->name() != std::string("binding")) {
+            errorOut << "Unexpected tag " << std::quoted(bindingNode->name()) << std::endl;
+            return std::nullopt;
+        }
+
+        Binding binding;
+        auto variableNode = bindingNode->first_node();
+        do {
+            if (variableNode->name() != std::string("variable")) {
+                errorOut << "Unexpected tag " << std::quoted(variableNode->name()) << std::endl;
+                return std::nullopt;
+            }
+
+            if (variableNode->first_attribute("id") == nullptr) {
+                errorOut << "Missing id attribute on variable tag" << std::endl;
+                return std::nullopt;
+            }
+
+            const auto variableId = variableNode->first_attribute("id")->value();
+            const auto variableIndexIt = _builder.getVariableIndices()->find(variableId);
+            if (variableIndexIt == _builder.getVariableIndices()->end()) {
+                errorOut << "Unknown variable id " << std::quoted(variableId) << std::endl;
+                return std::nullopt;
+            }
+
+            const auto colorNode = variableNode->first_node();
+            if (colorNode->name() != std::string("color")) {
+                errorOut << "Unexpected tag " << std::quoted(colorNode->name()) << std::endl;
+                return std::nullopt;
+            }
+
+            if (colorNode->next_sibling() != nullptr) {
+                errorOut << "Variables cannot be bound to product colors" << std::endl;
+                return std::nullopt;
+            }
+
+            if (colorNode->type() != rapidxml::node_element) {
+                errorOut << "Expected color tag to only contain color id" << std::endl;
+                return std::nullopt;
+            }
+
+            const auto colorIndex = _findColorIndex(_builder.getUnderlyingVariableColorTypes()[variableIndexIt->second], colorNode->value());
+            if (!colorIndex.has_value()) {
+                errorOut << "Unknown color " << std::quoted(colorNode->value()) << " for variable " << std::quoted(variableId) << std::endl;
+                return std::nullopt;
+            }
+
+            binding.setValue(variableIndexIt->second, *colorIndex);
+        } while ((variableNode = variableNode->next_sibling()) != nullptr);
+
+        return std::make_pair(transitionIndexIt->second, binding);
+    }
+
+    void ExplicitColoredInteractiveMode::_printCurrentMarking(
+        std::ostream &out,
+        const ColoredPetriNetMarking &currentMarking
+    ) const {
+        out << "<marking>" << std::endl;
+        for (Place_t place = 0; place < currentMarking.markings.size(); place++) {
+            out << "\t<place id=" << std::quoted(_builder.getPlaceName(place)) << ">" << std::endl;
+            const auto& colorCodec = _cpn.getPlaces()[place].colorType->colorCodec;
+            const auto& colorType = *_builder.getPlaceUnderlyingColorType(place);
+            for (const auto& [encodedColor, count] : currentMarking.markings[place].counts()) {
+                if (count > 0) {
+                    out << "\t\t<token count=" << std::quoted(std::to_string(count)) << ">" << std::endl;
+                    if (colorCodec.getColorCount() > 1) {
+                        const auto productColorType = dynamic_cast<const Colored::ProductType*>(&colorType);
+                        if (productColorType == nullptr) {
+                            throw std::runtime_error("Color codec is inconsistent with underlying color type");
+                        }
+                        for (size_t colorIndex = 0; colorIndex < colorCodec.getColorCount(); colorIndex++) {
+                            const Color_t color = colorCodec.decode(color, colorIndex);
+                            out << "\t\t\t<color>"
+                                << (*(productColorType->getNestedColorType(colorIndex)))[color].getColorName()
+                                << "</color>"
+                                << std::endl;
+                        }
+                    } else {
+                        out << "\t\t\t<color>"
+                            << colorType[encodedColor].getColorName()
+                            << "</color>"
+                            << std::endl;
+                    }
+                    out << "\t\t</token>" << std::endl;
+                }
+            }
+            out << "\t</place>" << std::endl;
+        }
+        out << "</marking>" << std::endl;
     }
 }
